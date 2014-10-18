b75565f3971af81e7c1e58f1d10c7d6b0ab33bb0
Pete Bachant <petebachant@gmail.com>
2014-10-18 12:16:28 -0400

    Try using struct module to unpack data, and use idn as a property

--- micronopt.py
+++ micronopt.py
@@ -5,6 +5,7 @@ This module is for working with Micron Optics interrogators.
 """
 from __future__ import print_function, division
 import socket
+import struct
 import sys
 
 if sys.version_info >= (3, 0):
@@ -18,21 +19,37 @@ class MicronInterrogator(object):
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.latest_response = ""
+    
     def connect(self):
         self.socket.connect((self.ip_address, self.port))
-    def send_command(self, command, buffsize=1024):
+
+    def send_command(self, command):
         if command[0] != "#":
             command = "#" + command
         if command[-1] != "\n":
             command += "\n"
         self.socket.send(command.encode("ascii"))
-        self.latest_response = socket.recv(buffsize)
-    def get_id(self, verbose=True):
-        self.send_command("ID?")
-        print(self.latest_response)
+        respsize = int(self.socket.recv(10))
+        self.latest_response = self.socket.recv(respsize)
+
+    @property
+    def idn(self):
+        self.send_command("IDN?")
+        return self.latest_response.decode()
+
     def get_data(self):
         self.send_command("GET_DATA")
+        self.data = self.latest_response
+        print(self.data[0:7])
+        print(struct.unpack("8b", self.data[1]))
 
+    def disconnect(self):
+        self.socket.close()
+
+def test_class():
+    interr = MicronInterrogator()
+    interr.connect()
+    print(interr.idn)
 
 def test():
     ip_address = "10.0.0.126"
@@ -50,4 +67,4 @@ def test():
     s.close()
 
 if __name__ == "__main__":
-    test()
+    test_class()
