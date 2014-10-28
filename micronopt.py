cf12944d336109bc30f3006282d09359660b426b
Pete Bachant <petebachant@gmail.com>
2014-10-28 13:27:33 -0400

    Add function to get serial no

--- micronopt.py
+++ micronopt.py
@@ -10,7 +10,7 @@ import sys
 
     
 class MicronInterrogator(object):
-    def __init__(self, ip_address="10.0.0.126", port=1852):
+    def __init__(self, ip_address="192.168.1.166", port=1852):
         self.ip_address = ip_address
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,15 +32,23 @@ class MicronInterrogator(object):
     def idn(self):
         self.send_command("IDN?")
         return self.latest_response.decode()
+        
+    @property
+    def serial_no(self):
+        self.send_command("GET_SN")
+        return self.latest_response.decode()
 
     def get_data(self):
         self.send_command("GET_DATA")
         data = self.latest_response
         # unpack the struct into variables
         (
-            abcde, fw_ver, cur_layer, fs_radix,  # 0 needs parse
-            fghij, knpl, fbg_thermistor,  # 1 needs parse
-            tx_ambient_temp, reserved2,  # 2
+            fs_radix, cur_layer, fw_ver, abcde,
+#            abcde, fw_ver, cur_layer, fs_radix,  # 0 needs parse
+            fbg_thermistor, knpl, fghij,
+#            fghij, knpl, fbg_thermistor,  # 1 needs parse
+            reserved2, tx_ambient_temp,
+#            tx_ambient_temp, reserved2,  # 2
             num_ffpi_peaks, num_fbg_peaks,  # 3
             num_dut2_peaks, num_dut1_peaks,  # 4
             num_dut4_peaks, num_dut3_peaks,  # 5
@@ -50,7 +58,8 @@ class MicronInterrogator(object):
             kernel_timestamp_seconds,  # 9
             kernel_src_buffer, kernel_buffers,  # 10
             error_and_kernel_rt_loc0,  # 11 needs parse
-            header_length, header_ver, buffers,  # 12
+            header_ver, header_length, buffer,
+#            header_length, header_ver, buffer,  # 12
             dut2_gain, dut1_gain,  # 13
             dut4_gain, dut3_gain,  # 14
             dut2_noise_thresh, dut1_noise_thresh,  # 15
@@ -61,7 +70,7 @@ class MicronInterrogator(object):
             starting_lambda,  # 20
             ending_lambda  # 21
         ) = struct.unpack(
-            '>'  # big endian
+            '<'  # big endian
             'BBBB'  # 0 needs parse
             'BBH'  # 1 needs parse
             'HH'  # 2
@@ -86,13 +95,16 @@ class MicronInterrogator(object):
             'I',  # 21
             data
         )
-        
+        print(data)
+        print(hex(abcde))
+        print(abcde & 0x10)
         # 0 parse abcde
         acq_triggered = bool(abcde & 0x80)
         calibration_fault = bool(abcde & 0x40)
         start_of_frame = bool(abcde & 0x20)
         primary_fan_state = bool(abcde & 0x10)
         secondary_fan_state = bool(abcde & 0x08)
+        
         s0_mux_state = bool(abcde & 0x04)
         s1_mux_state = bool(abcde & 0x02)
         s2_mux_state = bool(abcde & 0x01)
@@ -119,18 +131,33 @@ class MicronInterrogator(object):
         kernel_rt_loc0 = error_and_kernel_rt_loc0 & 0xffffff
         
         print("Serial number", serial_number)
-        
+        print("FBG thermistor:", fbg_thermistor)
+        print("FS radix:", fs_radix)
         print("Firmware version:", fw_ver)
         print("Acquisition triggered:", acq_triggered)
         print("Calibration fault:", calibration_fault)
+        print("start_of_frame:", start_of_frame)
         print("Primary fan state:", primary_fan_state)
         print("Secondary fan state:", secondary_fan_state)
+        print("s0_mux_state:", s0_mux_state)
+        
+        print("Percent buffer:", buffer)
         
         print("Header length:", header_length)
+        print("Header version:", header_ver)
         
         print("Tx ambient temp:", tx_ambient_temp)
         
+        print("sm041_mux_level:", sm041_mux_level)
+        
+        print("hw_clk_div:", hw_clk_div)
+        
+        print("Granularity:", granularity)
+        
         print("Operating mode:", operating_mode)
+        
+        print("Starting lambda:", starting_lambda)
+        print("Ending lambda:", ending_lambda)
 
     def disconnect(self):
         self.socket.close()
@@ -141,9 +168,9 @@ def test_class():
     interr.connect()
     print(interr.idn)
     interr.get_data()
+    print(interr.serial_no)
 
-
-def terminal(ip_address="10.0.0.126", port=1852):
+def terminal(ip_address="192.168.1.166", port=1852):
     """Creates a communcation terminal to send commands."""
     if sys.version_info >= (3, 0):
         input_function = input
