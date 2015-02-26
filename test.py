9d3671023316decf40435ad24a63f84b2f376923
Pete Bachant <petebachant@gmail.com>
2015-02-26 18:39:33 -0500

    Add test for num_acq

--- test.py
+++ test.py
@@ -76,6 +76,39 @@ def test_continuous_hwtrigger(test_dur=3):
     interr.disconnect()
     return data
     
+def test_num_acq_hwtrigger(test_dur=3):
+    import matplotlib.pyplot as plt
+    interr = Interrogator()
+    interr.connect()
+    interr.flush_buffer()
+    interr.create_sensors_from_file("test/fbg_properties.json")
+    interr.trig_mode = 3
+    interr.trig_start_edge = 1
+    interr.trig_stop_type = 0
+    interr.trig_num_acq = 60000
+    interr.trig_stop_edge = 0
+    interr.auto_retrig = 1
+    interr.zero_strain_sensors()
+    data = interr.data
+    interr.setup_append_data()
+    t0 = time.time()
+    while time.time() - t0 < test_dur:
+        interr.get_data()
+        interr.sleep()
+    t = data["time"]
+    data2 = data[interr.sensors[1].name + "_strain"]
+    try:
+        data2 -= data2[0]
+    except IndexError:
+        pass
+    plt.figure()
+    plt.plot(t, data2)
+    plt.xlabel("t (s)")
+    plt.ylabel("T (deg. C)")
+    print(interr.data_header)
+    interr.disconnect()
+    return data
+    
 def test_streaming(test_dur=2):
     import matplotlib.pyplot as plt
     interr = Interrogator()
@@ -125,3 +158,4 @@ if __name__ == "__main__":
 #    data = test_continuous(3)
 #    data = test_streaming(10)
     test_continuous_hwtrigger(10)
+#    test_num_acq_hwtrigger(10)
